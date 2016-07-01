667f739931c963a0034097393a336aa27c5cd1dd
vaibhavirana <vaibhavi9.rana@yahoo.com>
2016-07-01 17:34:33 +0530

    fgvfg fgdf

--- app/src/main/java/com/vebs/healthcare/MainActivity.java
+++ app/src/main/java/com/vebs/healthcare/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(new PagerAdapter() {
             @Override
             public int getCount() {
-                return 5;
+                return 3;
             }
 
             @Override
@@ -49,62 +49,40 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public Object instantiateItem(final ViewGroup container, final int position) {
                 final View view = LayoutInflater.from(
-                        getBaseContext()).inflate(R.layout.item_vp_list, null, false);
-
-                final RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.rv);
-                recyclerView.setHasFixedSize(true);
-                recyclerView.setLayoutManager(new LinearLayoutManager(
-                                getBaseContext(), LinearLayoutManager.VERTICAL, false
-                        )
-                );
-                recyclerView.setAdapter(new RecycleAdapter());
+                        getBaseContext()).inflate(R.layout.item_vp, null, false);
 
                 container.addView(view);
                 return view;
             }
         });
 
-        final String[] colors = getResources().getStringArray(R.array.default_preview);
 
         final NavigationTabBar navigationTabBar = (NavigationTabBar) findViewById(R.id.ntb_horizontal);
         final ArrayList<NavigationTabBar.Model> models = new ArrayList<>();
         models.add(
-                new NavigationTabBar.Model.Builder(
-                        getResources().getDrawable(R.drawable.ic_first),
-                        Color.parseColor(colors[0]))
-                        .title("Heart")
+                new NavigationTabBar.Model
+                        .Builder(
+                        getResources().getDrawable(R.drawable.ic_first),0)
+                        .title("DOCTOR")
                         .build()
         );
         models.add(
                 new NavigationTabBar.Model.Builder(
                         getResources().getDrawable(R.drawable.ic_second),
-                        Color.parseColor(colors[1]))
-                        .title("Cup")
+                        0)
+                        .title("LABS")
                         .build()
         );
         models.add(
                 new NavigationTabBar.Model.Builder(
                         getResources().getDrawable(R.drawable.ic_third),
-                        Color.parseColor(colors[2]))
-                        .title("Diploma")
+                        0)
+                        .title("DIAGNOSTIC")
                         .build()
         );
-        models.add(
-                new NavigationTabBar.Model.Builder(
-                        getResources().getDrawable(R.drawable.ic_fourth),
-                        Color.parseColor(colors[3]))
-                        .title("Flag")
-                        .build()
-        );
-        models.add(
-                new NavigationTabBar.Model.Builder(
-                        getResources().getDrawable(R.drawable.ic_fifth),
-                        Color.parseColor(colors[4]))
-                        .title("Medal")
-                        .build()
-        );
+
         navigationTabBar.setModels(models);
-        navigationTabBar.setViewPager(viewPager, 2);
+        navigationTabBar.setViewPager(viewPager, 0);
 
         navigationTabBar.post(new Runnable() {
             @Override
@@ -128,52 +106,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        findViewById(R.id.mask).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View v) {
-                for (int i = 0; i < navigationTabBar.getModels().size(); i++) {
-                    final NavigationTabBar.Model model = navigationTabBar.getModels().get(i);
-                    navigationTabBar.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            final String title = String.valueOf(new Random().nextInt(15));
-                            if (!model.isBadgeShowed()) {
-                                model.setBadgeTitle(title);
-                                model.showBadge();
-                            } else model.updateBadgeTitle(title);
-                        }
-                    }, i * 100);
-                }
-            }
-        });
+
     }
 
-    public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.ViewHolder> {
 
-        @Override
-        public ViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
-            final View view = LayoutInflater.from(getBaseContext()).inflate(R.layout.item_list, parent, false);
-            return new ViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(final ViewHolder holder, final int position) {
-            holder.txt.setText(String.format("Navigation Item #%d", position));
-        }
-
-        @Override
-        public int getItemCount() {
-            return 20;
-        }
-
-        public class ViewHolder extends RecyclerView.ViewHolder {
-
-            public TextView txt;
-
-            public ViewHolder(final View itemView) {
-                super(itemView);
-                txt = (TextView) itemView.findViewById(R.id.txt_vp_item_list);
-            }
-        }
-    }
 }
